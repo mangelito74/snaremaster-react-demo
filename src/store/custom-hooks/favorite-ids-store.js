98a3db7f2c6ee01c0652317fae16287e3e56397c
mangelito74 <mangelito74@gmail.com>
2023-09-21 09:53:59 +0200

    Saving favorites in local storage

--- src/store/custom-hooks/favorite-ids-store.js
+++ src/store/custom-hooks/favorite-ids-store.js
@@ -1,6 +1,11 @@
 import { initStore } from "./store";
 
 const configureStore = () => {
+  const storedUserFavoriteIds = localStorage.getItem("favoriteIds");
+  let favoriteIds = [];
+  if (storedUserFavoriteIds !== null) {
+    favoriteIds = storedUserFavoriteIds.split(",");
+  }
 
   const actions = {
     TOGGLE_FAVORITE: (curState, id) => {
@@ -10,11 +15,14 @@ const configureStore = () => {
       } else {
         curState.favoriteIds = curState.favoriteIds.filter((fid) => fid !== id);
       }
+
+      localStorage.setItem("favoriteIds", curState.favoriteIds);
+
       return { favoriteIds: curState.favoriteIds };
     },
   };
   initStore(actions, {
-    favoriteIds: [],
+    favoriteIds: favoriteIds,
   });
 };
 
