bc610f453b060bdc4d791f66fd1be25bc1138d96
mangelito74 <mangelito74@gmail.com>
2023-10-05 11:27:09 +0200

    Added logged in check to Favorites page

--- src/components/Favorites/FavoritesList.js
+++ src/components/Favorites/FavoritesList.js
@@ -1,28 +1,37 @@
+import { useContext } from "react";
+
 import Card from "../UI/Card";
 import FavoriteItem from "./FavoriteItem";
+
+import AuthenticationContext from "../../store/context-api/authentication-context";
 
 import classes from "./FavoritesList.module.css";
 
 const FavoritesList = (props) => {
   const favorites = props.favorites;
 
-  let content = <p className="placeholder">You have no favorites yet!</p>;
+  let content = <p className="placeholder">Log in to manage your favorites!</p>;
 
-  if (favorites.length > 0) {
-    content = (
-      <ul>
-        {favorites.map((favorite) => (
-          <FavoriteItem
-            key={favorite.id}
-            id={favorite.id}
-            image={favorite.image}
-            name={favorite.name}
-            description={favorite.description}
-            price={favorite.price}
-          />
-        ))}
-      </ul>
-    );
+  const authenticationContext = useContext(AuthenticationContext);
+  if (authenticationContext.isLoggedIn) {
+    if (favorites.length > 0) {
+      content = (
+        <ul>
+          {favorites.map((favorite) => (
+            <FavoriteItem
+              key={favorite.id}
+              id={favorite.id}
+              image={favorite.image}
+              name={favorite.name}
+              description={favorite.description}
+              price={favorite.price}
+            />
+          ))}
+        </ul>
+      );
+    } else {
+      content = <p className="placeholder">You have no favorites yet!</p>;
+    }
   }
 
   return (
